cea70bfcc9f19b6e5cda001a55d36af98e3863eb
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2024-04-04 13:47:35 -0500

    Implement requirements in code

---
+++ src/test/java/edu/miu/cs489/lab03/Lab03ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.lab03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
